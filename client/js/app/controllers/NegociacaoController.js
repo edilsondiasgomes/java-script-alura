ff9bf040a5c9f1a9305b468a2f56377dee541044
edy <edilsondiasgomes@gmail.com>
2019-11-06 15:08:16 -0300

    classe ListaNegociaçoes

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -11,12 +11,8 @@ class NegociacaoController {
 
     adiciona(event) {
         event.preventDefault();
-
         this._listaNegociacoes.adiciona(this._criaNegociacao());
         this._limpaFormulario();
-
-        console.log(this._listaNegociacoes.negociacoes);
-
     }
 
     _criaNegociacao() {
@@ -30,7 +26,6 @@ class NegociacaoController {
         this._inputData.value = '';
         this._inputQuantidade.value = 1;
         this._inputValor.value = 0.0;
-
         this._inputData.focus();
     }
 }
