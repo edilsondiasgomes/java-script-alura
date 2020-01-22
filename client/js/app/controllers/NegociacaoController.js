36e097fd4f1824b2989ab9f4ed509863aa663e4c
edy <edilsondiasgomes@gmail.com>
2020-01-22 09:00:22 -0300

    adicionado metodo de apagar negociacao

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -7,7 +7,7 @@ class NegociacaoController {
         this._inputData = $('#data');
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
-        this._listaNegociacoes = new ListaNegociacoes();
+        this._listaNegociacoes = new ListaNegociacoes(model => this._negociacoesView.update(model));
 
         this._negociacoesView = new NegociacoesView($('#negociacoesView'));
         this._negociacoesView.update(this._listaNegociacoes);
@@ -27,6 +27,13 @@ class NegociacaoController {
         this._mensagemView.update(this._mensagem);
 
         this._limpaFormulario();
+    }
+
+    apaga() {
+        this._listaNegociacoes.esvazia();
+        this._negociacoesView.update(this._listaNegociacoes);
+        this._mensagem.texto = "Negociações apagadas com sucesso";
+        this._mensagemView.update(this._mensagem);
     }
 
     _criaNegociacao() {
